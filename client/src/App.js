5a5e6dff51ca03edbf91b36b12143826d7c2fcd4
kimD0ngjun <chickentasty0112@gmail.com>
2023-08-12 18:14:53 +0900

    [commit] 로그인 기능 2) 로그인 디자인

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import OAuth from "./components/OAuth";
+import Login from "./components/Login";
 
 function App() {
   return (
     <>
       <OAuth />
+      <Login />
     </>
   );
 }
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <>
+      <div className="login box">
+        <div className="emailpart">
+          <div className="text">Email</div>
+          <input className="inputprofile" placeholder="Enter the email"></input>
+          <div className="inaccurateemail warningmsg">
+            이메일 형식이 올바르지 않습니다
+          </div>
+          <div className="incorrectemail warningmsg">
+            이메일이 일치하지 않습니다
+          </div>
+        </div>
+        <div className="passwordpart">
+          <div className="textgroup">
+            <div className="text">Password</div>
+            <div className="findpassword">Forget Password?</div>
+          </div>
+          <input
+            className="inputprofile"
+            placeholder="Enter the password"
+          ></input>
+          <div className="incorrectpwd warningmsg">
+            비밀번호가 일치하지 않습니다
+          </div>
+        </div>
+        <div className="loginbutton">Log in</div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
