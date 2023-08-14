a7ccebcafdbb298047779b3dbd4a8e92aae99d38
kimD0ngjun <chickentasty0112@gmail.com>
2023-08-15 01:13:54 +0900

    [commit] 로그인 기능 7) axios 문법 적용

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import "./Login.css";
 import { useDispatch } from "react-redux";
 import { login } from "../features/userSlice";
+import axios from "axios";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [invalidEmail, setInvalidEmail] = useState(false);
   const [pwd, setPwd] = useState("");
+  const [loginError, setLoginError] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -23,28 +25,49 @@ function Login() {
     return emailRegex.test(email);
   };
 
-  const handleLogin = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     if (!isValidEmail(email)) {
       setInvalidEmail(true);
+      return; // 유효하지 않은 이메일일 경우 로그인 중단
     } else {
       setInvalidEmail(false);
     }
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(
-      login({
-        email: email,
-        password: pwd,
-        loggedIn: true,
-      })
-    );
+    let body = {
+      email,
+      password: pwd,
+    };
+
+    try {
+      // Axios를 사용하여 서버로 POST 요청 전송
+      // axios.post 메서드가 http 요청(request) 보내주는 메소드임을 잊지 말자
+      const response = await axios.post("/user/login", body);
+
+      // 서버에서 받아온 토큰 추출
+      const token = response.data.token;
+
+      // 서버에서 받아온 토큰을 사용하여 Redux 상태 업데이트
+      dispatch(
+        login({
+          email: email,
+          token: token,
+          loggedIn: true,
+        })
+      );
+
+      // 로그인 성공 시 다른 화면으로 이동하거나 상태를 변경하는 로직 추가 필요
+      // 예시: history.push("/dashboard");
+      // 지금은 단순히 메시지 띄우는 정도로
+    } catch (error) {
+      console.error("Login error:", error);
+      setLoginError(true);
+    }
   };
 
   return (
     <>
-      <form className="login box" onSubmit={(event) => handleSubmit(event)}>
+      <form className="login box" onSubmit={handleSubmit}>
         <div className="emailpart">
           <div className="guide">
             <div className="text">Email</div>
@@ -78,14 +101,12 @@ function Login() {
             onChange={handlePwdChange}
           ></input>
           <div className="warn">
-            <div className="incorrect warningmsg">
+            <div className={`incorrect warningmsg ${loginError ? "show" : ""}`}>
               일치하는 회원정보가 없습니다
             </div>
           </div>
         </div>
-        <button className="loginbutton" onClick={handleLogin}>
-          Log in
-        </button>
+        <button className="loginbutton">Log in</button>
       </form>
     </>
   );
