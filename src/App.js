ae8a4b2317e4202b72cfbf1eed4413985f5b8861
yuumni <minyunj77@gmail.com>
2023-08-16 15:18:35 +0900

    게시글 컴포넌트 틀 제작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { Header, SearchDropdown } from './components/Header';
+import Questions from './pages/Questions';
+import Question from './components/Question';
 
 function App() {
   return (
     <>
       <Header />
+      
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      
     </>
     
   );
